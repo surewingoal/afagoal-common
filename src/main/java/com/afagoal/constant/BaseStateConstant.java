4ce7a88ec6de6ec5daacd19664f784f504b2e9b8
xiaxi <xiaxi@jianbaolife.com>
2018-02-27 15:49:24 +0800

    baseState

---
+++ src/main/java/com/afagoal/constant/BaseStateConstant.java
@@ -0,0 +1,11 @@
+package com.afagoal.constant;
+
+/**
+ * Created by BaoCai on 18/2/27.
+ * Description:
+ */
+public class BaseStateConstant {
+
+    public static final Byte DEFAULT_STATE = 0;
+
+}
