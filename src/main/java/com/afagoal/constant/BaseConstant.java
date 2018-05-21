9a38751ffe8653b889974a052624493531fad092
xiaxi <xiaxi@jianbaolife.com>
2018-05-21 13:56:27 +0800

    baseConstant

--- src/main/java/com/afagoal/constant/BaseConstant.java
+++ src/main/java/com/afagoal/constant/BaseConstant.java
@@ -4,10 +4,12 @@ package com.afagoal.constant;
  * Created by BaoCai on 18/2/27.
  * Description:
  */
-public class BaseStateConstant {
+public class BaseConstant {
 
     public static final Byte DEFAULT_STATE = 0;
 
     public static final Byte DELETE_STATE = 99;
+
+    public static final String SUPERADMIN = "superadmin";
 
 }
